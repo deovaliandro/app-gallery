48b96ac73492ab79f7cd4124e32e2801065870d3
deovaliandro <mdv16h@student.unhas.ac.id>
2020-12-13 06:01:08 +0800

    update README.md

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -74,8 +74,9 @@
         <div class="container is-fluid">
             <div class="columns">
                 <div class="column is-1"></div>
-                <div class="column">Maximas vero virtutes iacere omnis necesse est voluptate dominante. Negat esse eam, inquit, propter se expetendam. Eam stabilem appellas. Equidem etiam Epicurum, in physicis quidem, Democriteum puto. Plane idem, inquit, et maxima quidem, qua fieri nulla maior potest. Equidem, sed audistine modo de Carneade? Teneo, inquit, finem illi videri nihil dolere. Quid ergo attinet gloriose loqui, nisi constanter loquare?
-
+                <div class="column">
+                    Maximas vero virtutes iacere omnis necesse est voluptate dominante. Negat esse eam, inquit, propter se expetendam. Eam stabilem appellas. Equidem etiam Epicurum, in physicis quidem, Democriteum puto. Plane idem, inquit, et maxima quidem, qua fieri nulla maior potest. Equidem, sed audistine modo de Carneade? Teneo, inquit, finem illi videri nihil dolere. Quid ergo attinet gloriose loqui, nisi constanter loquare?
+                    <br>
                     Quia voluptatem hanc esse sentiunt omnes, quam sensus accipiens movetur et iucunditate quadam perfunditur.
                     Sit hoc ultimum bonorum, quod nunc a me defenditur;
                     Est autem situm in nobis ut et adversa quasi perpetua oblivione obruamus et secunda iucunde ac suaviter meminerimus.
